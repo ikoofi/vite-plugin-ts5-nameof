2b9064453aef4edf4d59b59b06775836be4e3b81
kornilaevvv <vladkornilaev@mail.ru>
2024-01-24 09:15:34 +0300

    d

--- example/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tsNameof from 'vite-plugin-ts-nameof'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [tsNameof(), vue()]
-})
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import tsNameof from 'ts-nameof';
+import tsNameof from 'ts5-nameof';
 import type { Plugin } from 'vite';
 
 export default (): Plugin => ({
-  name: 'vite-plugin-ts-nameof',
+  name: 'vite-plugin-ts5-nameof',
   enforce: 'pre',
   transform(code, id) {
     if (/\.(vue|[mc]?ts)/i.test(id) && code.includes('nameof')) {
